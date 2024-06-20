021d4eaeccb1428577d403251bfb7a99ef643cc2
BiliBoard-Project <biliboard_china@outlook.com>
2024-06-20 23:23:35 +0800

    update

---
+++ script.js
@@ -0,0 +1,5 @@
+function isMobileDevice() {
+    let userAgent = navigator.userAgent || navigator.vendor || window.opera;
+    return /android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(userAgent.toLowerCase());
+};
+
